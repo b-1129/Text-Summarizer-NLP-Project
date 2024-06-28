2929d145dc5cfc463d1ade5026da2eb029c9a46e
Brijesh Kapadiya <brijesh29.it11@gmail.com>
2024-06-28 23:27:07 +0530

    model evaluation completed

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,7 +1,10 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
 from textSummarizer.entity import (DataIngestionConfig, 
-                                   DataValidationConfig)
+                                   DataValidationConfig,
+                                   DataTransformationConfig,
+                                   ModelTrainerConfig,
+                                   ModelEvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -40,3 +43,65 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config= DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name=config.tokenizer_name
+        )
+        return data_transformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.get('TrainingArguments', {})
+
+        # Provide default values if necessary
+        num_train_epochs = params.get('num_train_epochs', 3)
+        warmup_steps = params.get('warmup_steps', 500)
+        per_device_train_batch_size = params.get('per_device_train_batch_size', 1)
+        weight_decay = params.get('weight_decay', 0.01)
+        logging_steps = params.get('logging_steps', 10)
+        evaluation_strategy = params.get('evaluation_strategy', 'steps')
+        eval_steps = params.get('eval_steps', 500)
+        save_steps = params.get('save_steps', 1e6)
+        gradient_accumulation_steps = params.get('gradient_accumulation_steps', 16)
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_ckpt=config.model_ckpt,
+            num_train_epochs=num_train_epochs,
+            warmup_steps=warmup_steps,
+            per_device_train_batch_size=per_device_train_batch_size,
+            weight_decay=weight_decay,
+            logging_steps=logging_steps,
+            evaluation_strategy=evaluation_strategy,
+            eval_steps=eval_steps,
+            save_steps=save_steps,
+            gradient_accumulation_steps=gradient_accumulation_steps
+        )
+
+        return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config= self.config.model_evaluation
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir= config.root_dir,
+            data_path= config.data_path,
+            model_path= config.model_path,
+            tokenizer_path= config.tokenizer_path,
+            metric_file_name= config.metric_file_name
+        )
+
+        return model_evaluation_config
